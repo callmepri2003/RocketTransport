6e0897e7352c1188694e24ad94f4f783060eec44
callmepri <140776053+callmepri@users.noreply.github.com>
2025-10-06 08:18:20 +1100

    feat: restarting structure

--- react-bricks/bricks/index.ts
+++ react-bricks/bricks/index.ts
@@ -1,29 +1,7 @@
 import { types } from "react-bricks/frontend"
-import Hero from "./Hero"
-import { Services } from "./Services"
-import { StatItem, Stats } from "./Stats"
-import ServiceItem from "./ServiceItem"
-import { Navigation, NavItem } from "./Navigation"
-import { About, AboutFeature } from "./About"
-import { CTA, CTAItem } from "./CTA"
-import { Footer, FooterColumn, FooterContactItem, FooterLink } from "./Footer"
 
 const bricks: types.Brick[] = [
-  Hero,
-  Services,
-  Stats,
-  StatItem,
-  ServiceItem,
-  Navigation,
-  NavItem,
-  About,
-  AboutFeature,
-  CTA,
-  CTAItem,
-  Footer,
-  FooterColumn,
-  FooterLink,
-  FooterContactItem,
+
 ]
 
 export default bricks
