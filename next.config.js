472bb5d5e93b36c7269cb2aba533f08f1e3d74a1
callmepri <140776053+callmepri@users.noreply.github.com>
2025-10-18 17:24:23 +1100

    feat: breh

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   i18n: {
-    locales: ['en'], // Add your languages here
+    locales: ['en'],
     defaultLocale: 'en',
     localeDetection: false,
-  },
-
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/home',
-        permanent: true, // set to false if you might change your homepage later
-      },
-    ];
   },
 };
