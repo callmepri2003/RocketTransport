c8250329fb0962487ae03a0b03a3f1fa8616f818
callmepri <140776053+callmepri@users.noreply.github.com>
2025-10-20 08:51:00 +1100

    feat: sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.rockettransport.au',
+  generateRobotsTxt: true, // creates robots.txt
+  sitemapSize: 7000, // optional
+};
