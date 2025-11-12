ca31639b00fe8de7b07983a02b36aa890b87e85d
callmepri <140776053+callmepri@users.noreply.github.com>
2025-11-12 14:43:36 +1100

    feat: updated seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,6 +21,34 @@ export default function Document() {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
         />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "LocalBusiness",
+              "name": "Rocket Transport Pty Ltd",
+              "description": "Rocket Transport is an independent freight company specialising in all types of pallet goods services. Trusted since 2001, we're an established player in the industry and can guarantee that you'll be satisfied with our service.",
+              "telephone": "+61 (02) 402 874 008",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "8 Shropshire Cl",
+                "addressLocality": "Wakeley",
+                "addressRegion": "NSW",
+                "postalCode": "2176",
+                "addressCountry": "AU"
+              },
+              "url": "https://rockettransport.au/",
+              "openingHours": "Mo-Fr 08:00-18:00",
+              "geo": {
+                "@type": "GeoCoordinates",
+                "latitude": -33.90,
+                "longitude": 151.02
+              }
+            }),
+          }}
+        />
+
       </Head>
 
       <body>
